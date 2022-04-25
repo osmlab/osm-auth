48715dd42e6904e5f0815c7e9ecd819641c56731
Bryan Housel <bhousel@gmail.com>
2022-04-25 11:30:42 -0400

    Swap ecstatic (deprecated) for http-server

--- server.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-var http = require('http');
-var ecstatic = require('ecstatic');
-
-http.createServer(
-    ecstatic({ root: __dirname, cache: 0 })
-).listen(8080);
-
-console.log('Listening on :8080');
