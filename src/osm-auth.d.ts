a0ba5fe462812fc905e868f7fec26235c0c223c6
Bryan Housel <bhousel@gmail.com>
2022-04-27 09:44:10 -0400

    Update .d.ts declaration file

--- src/osm-auth.d.ts
+++ src/osm-auth.d.ts
@@ -1,56 +1,48 @@
-// Type definitions for osm-auth 2
+// Type definitions for osm-auth
 
 declare namespace OSMAuth {
-    interface OSMAuthConstructor {
-        new (options: OSMAuthOptions): OSMAuthInstance;
-    }
+  interface OSMAuthConstructor {
+    new (options: OSMAuthOptions): OSMAuthInstance;
+  }
 
-    interface OSMAuthInstance {
-        popupWindow?: Window
-        logout(): OSMAuthInstance;
-        authenticated(): boolean;
-        authenticate(callback: (error: null | ErrorEvent | XMLHttpRequest, oauth?: OSMAuthInstance) => any): any;
-        bringPopupWindowToFront(): boolean;
-        xhr(options: OSMAuthXHROptions, callback: (error: null | ErrorEvent | XMLHttpRequest, xhr: any) => any): any;
-        options(): OSMAuthOptions;
-        options(options: OSMAuthNewOptions): OSMAuthInstance;
-        bootstrapToken(oauth_token: string, callback: (error: null | ErrorEvent | XMLHttpRequest, oauth?: OSMAuthInstance) => any): any;
-        preauth(options: OSMAuthOptions): OSMAuthInstance;
-    }
+  interface OSMAuthInstance {
+    popupWindow?: Window
+    logout(): OSMAuthInstance;
+    authenticated(): boolean;
+    authenticate(callback: (err: null | any, result?: any) => any): any;
+    bringPopupWindowToFront(): boolean;
+    bootstrapToken(oauth_token: string, callback: (err: null | any, result?: any) => any) => any): any;
+    xhr(options: OSMAuthXHROptions, callback: (err: null | any, result?: any) => any) => any): XMLHttpRequest | null;
+    rawxhr(method: string, url: string, access_token: string | null, data: any | null, headers: object | null, callback: (err: null | any, result?: any) => any) => any): XMLHttpRequest;
+    preauth(options: OSMAuthOptions): OSMAuthInstance;
+    options(): OSMAuthOptions;
+    options(options: OSMAuthOptions): OSMAuthInstance;
+  }
 
-    interface OSMAuthOptions {
-        oauth_consumer_key: string;
-        oauth_secret: string;
-        url?: string;
-        auto?: boolean;
-        loading?: () => any;
-        done?: () => any;
-        landing?: string;
-        singlepage?: boolean;
-    }
+  interface OSMAuthOptions {
+    scope?: string;
+    client_id?: string;
+    client_secret?: string;
+    redirect_uri?: string;
+    access_token?: string;
+    url?: string;
+    auto?: boolean;
+    singlepage?: boolean;
+    loading?: () => any;
+    done?: () => any;
+  }
 
-    interface OSMAuthNewOptions {
-        oauth_consumer_key?: string;
-        oauth_secret?: string;
-        url?: string;
-        auto?: boolean;
-        loading?: () => any;
-        done?: () => any;
-        landing?: string;
-        singlepage?: boolean;
-    }
-
-    interface OSMAuthXHROptions {
-        path: string;
-        method: 'POST' | 'PUT' | 'GET' | 'DELETE';
-        content?: string;
-        prefix?: boolean;
-        options?: any;
-    }
+  interface OSMAuthXHROptions {
+    method: 'POST' | 'PUT' | 'GET' | 'DELETE';
+    path: string;
+    content?: string;
+    prefix?: boolean;
+    headers?: object;
+  }
 }
 
 declare var osmAuth: OSMAuth.OSMAuthConstructor;
 
 declare module "osm-auth" {
-    export = osmAuth;
+  export = osmAuth;
 }
